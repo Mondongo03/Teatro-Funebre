da6657a0cc3b589dec5badf743f74b62b47759ef
ruben <rjulian@elpuig.xeill.net>
2024-02-20 11:37:48 +0100

    mjmj

--- .godot/mono/temp/obj/Debug/Proyecto Final.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Proyecto Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671a2146c55bb2ee42d30e5442dcec38f8f24a23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5251207663007f17ad9f32d8b6ac0c69f95dc420")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
