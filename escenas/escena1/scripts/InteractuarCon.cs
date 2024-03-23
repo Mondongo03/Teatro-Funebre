f20311e36dc0fbe35821766e3bcf468579f247da
ruben <rjulian@elpuig.xeill.net>
2024-03-23 20:30:50 +0100

    merge

--- escenas/escena1/scripts/InteractuarCon.cs
+++ escenas/escena1/scripts/InteractuarCon.cs
@@ -3,21 +3,22 @@ using System;
 
 public partial class InteractuarCon : Area2D
 {
-    Despertarse despertarse = new Despertarse();
+	Despertarse despertarse = new Despertarse();
 
-    private bool bolaInside = false;
-    private bool godotInside = false;
+	private bool bolaInside = false;
+	private bool godotInside = false;
 
 	public static Boolean clickado = false;
-
-<<<<<<< HEAD
 	public static int slotsOcupados = 0;
 	public override void _Ready(){
 		
 	}
 
 	public override void _Process(double delta){
-
+		if (Reloj.terminado)
+		{   
+			despertarse.despertar(true);
+		}
 	}
 
 	public void _on_hueco_area_entered(CollisionObject2D collisionObject2D){
@@ -34,6 +35,12 @@ public partial class InteractuarCon : Area2D
 			slotsOcupados++;
 			GD.Print(slotsOcupados);
 		GD.Print("Godot a entrau");
+
+		if (collisionObject2D.IsInGroup("personaje"))
+		{
+			GD.Print("Saul ha entrado al área. Cambiando de escena...");
+			GetTree().ChangeSceneToFile("res://escenas/escena2/Prueba.tscn");
+		}
 		}
 	}
 
@@ -54,50 +61,3 @@ public partial class InteractuarCon : Area2D
 	}
 	
 }
-=======
-    public override void _Ready()
-    {
-    }
-
-    public override void _Process(double delta)
-	{
-        if (bolaInside && godotInside)
-        {   
-            despertarse.despertar(true);
-        }
-	}
-
-    public void _on_hueco_area_entered(CollisionObject2D collisionObject2D)
-    {
-        if (collisionObject2D.IsInGroup("bola"))
-        {
-            GD.Print("Bola ha entrado al área.");
-            bolaInside = true;
-        }
-        else if (collisionObject2D.IsInGroup("godot"))
-        {
-            GD.Print("Godot ha entrado al área.");
-            godotInside = true;
-        }
-        else if (collisionObject2D.IsInGroup("personaje"))
-        {
-            GD.Print("Saul ha entrado al área. Cambiando de escena...");
-            GetTree().ChangeSceneToFile("res://escenas/escena2/Prueba.tscn");
-        }
-    }
-
-    public void _on_hueco_area_exited(CollisionObject2D collisionObject2D)
-    {
-        if (collisionObject2D.IsInGroup("bola"))
-        {
-            GD.Print("Bola ha salido del área.");
-            bolaInside = false;
-        }
-        else if (collisionObject2D.IsInGroup("godot"))
-        {
-            GD.Print("Godot ha salido del área.");
-            godotInside = false;
-        }
-    }
-}
->>>>>>> main
