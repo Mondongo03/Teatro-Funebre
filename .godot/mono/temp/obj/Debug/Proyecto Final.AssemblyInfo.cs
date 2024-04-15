9068a5d086cb41973eaf8e31919677919fcaf504
ruben <rjulian@elpuig.xeill.net>
2024-04-15 20:00:15 +0200

    Pistas

--- .godot/mono/temp/obj/Debug/Proyecto Final.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Proyecto Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbf4cfdad927891c8fcbcbdc14fd4257f33e2eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d252b48d4d1186da8ee7305ad9c55de8a0ca0b31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- escenas/escena1/Main.cs
+++ escenas/escena1/Main.cs
@@ -7,6 +7,7 @@ public partial class Main : Node2D {
 			InteractuarCon interactuarCon = new InteractuarCon();
 			Reloj reloj = new Reloj();
 			int slotsOcupados;
+			Area2D pista_1;
 			 Node2D godotInstancia2;
 			 public static Node2D varillaMinutosInstancia;
 			 public static Node2D varillaSegundosInstancia;
@@ -16,6 +17,7 @@ public partial class Main : Node2D {
 			 public static Node2D huecoInstancia;
 			 public static Node2D escaleraInstancia;
 			 public static Node2D tapaInstancia;
+			 public static Node2D pista_1Instancia;
 			 Node2D relojInstancia;
 			
 			 Boolean clickadoBola = false;
@@ -23,8 +25,6 @@ public partial class Main : Node2D {
 			public static Boolean varillaMinutosReloj = false;
 			public static Boolean varillaSegundosReloj = false;
 	public override void _Ready() {
-		
-			
 		InstanciarEscena();
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -58,6 +58,19 @@ public partial class Main : Node2D {
 			PackedScene tapa = (PackedScene)ResourceLoader.Load("res://escenas/escena1/objects/tapa.tscn");
 	    	tapaInstancia = tapa.Instantiate() as Node2D; 
 			AddChild(tapaInstancia);
+
+			PackedScene pista_1 = (PackedScene)ResourceLoader.Load("res://escenas/escena1/objects/pista_1.tscn");
+	    	pista_1Instancia = pista_1.Instantiate() as Node2D; 
+			AddChild(pista_1Instancia);
+	}
+
+	public void popPista_1(){
+		pista_1Instancia.Position = new Vector2I(334,255);
+	}
+
+	public void _on_timer_timeout(){
+		if(VarillaM.encontrado == false || VarillaS.encontrado == false){
+			popPista_1();}
 	}
 
 }
