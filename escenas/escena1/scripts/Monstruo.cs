1985b4612bb654e07ce2257b3ff3cff53380441d
hcatalan <hcatalan@elpuig.xeill.net>
2024-04-24 20:29:26 +0200

    MonstruoMovimiento

--- escenas/escena1/scripts/Monstruo.cs
+++ escenas/escena1/scripts/Monstruo.cs
@@ -3,29 +3,32 @@ using System;
 
 public partial class Monstruo : PathFollow2D
 {
-
+[Export]
+public AnimationPlayer animationPlayer;
 	float speed = 0.2f;
 	[Export]
 	private AnimatedSprite2D animatedSprite2D;
-	public static bool funciona = false;
+	public static bool encontrado = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if(funciona == true)
-		{
-			animatedSprite2D.Play("");
-			ProgressRatio += (float)delta * speed;
-		}
 	}
 
-	public void despertar(bool boolean)
-	{
-		funciona = boolean;
+	public void _on_input_event(Node viewport, InputEvent evento, int shap){
+	if(evento.IsActionPressed("click_izquierdo")){
+	if (encontrado){
+		animationPlayer.Play("MonstruoEscondido");
+		encontrado = true;
+	}
+	else{
+		animationPlayer.Play("MonstruoEncontrado");
+		encontrado = true;
 	}
 }
