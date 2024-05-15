2625a79c76965cff5de67a028f03b22360da8dab
ruben <rjulian@elpuig.xeill.net>
2024-05-15 20:29:59 +0200

    Animación al fallar funciona

--- escenas/escena2.5/CartasCartitas.cs
+++ escenas/escena2.5/CartasCartitas.cs
@@ -7,25 +7,35 @@ public partial class CartasCartitas : Node2D {
 	public static int[] cartas = { 0, 1, 2 };
 	public static int intento = 0;
 	public static bool victoria = false;
-	float speed = 0.3f;
+	float speed = 0.6f;
 	bool remenar;
+	Timer timer;
 	[Export] public PathFollow2D sol, estrella, luna;
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
 		remenar = true;
-
 		barajarCartas();
+		timer = new Timer();
+        AddChild(timer);
+        timer.OneShot = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+	public async override void _Process(double delta) {
+			if(remenar){
+			sol.ProgressRatio += (float)delta * speed;
+			estrella.ProgressRatio += (float)delta * speed;
+			luna.ProgressRatio += (float)delta * speed;
+			}
+
 		if(intento >0 && !victoria){
 			barajarCartas();
 			intento--;
-		}
-		if (remenar)
-		{
+			timer.Start(1);
+        	await ToSignal(timer, "timeout");
+			sol.ProgressRatio =0;
+			estrella.ProgressRatio =0;
+			luna.ProgressRatio =0;
 			sol.ProgressRatio += (float)delta * speed;
 			estrella.ProgressRatio += (float)delta * speed;
 			luna.ProgressRatio += (float)delta * speed;
