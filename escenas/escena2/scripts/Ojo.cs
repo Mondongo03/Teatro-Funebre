42799e61ee5778894bc39a4bf4ecd9d91ae6ebf9
Macnacka <93340013+BaenaArnau@users.noreply.github.com>
2024-05-15 18:48:50 +0200

    Comentarios

--- escenas/escena2/scripts/Ojo.cs
+++ escenas/escena2/scripts/Ojo.cs
@@ -1,37 +1,73 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Clase que nos permite gestionar como funciona el ojo del gnomo
+/// </summary>
 public partial class Ojo : Area2D
 {
 	Sprite2D sprite;
 	bool puedoMover = false;
-	public override void _Ready() {
+
+	/// <summary>
+	/// Esta funcion se llama automaticamente cuando se instancia el objeto al cual esta asociado el script
+	/// </summary>
+	public override void _Ready()
+	{
 		sprite = GetChild<Sprite2D>(0);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
-		if(puedoMover){
+	/// <summary>
+	/// Señal de godot que nos permite saber cuando el raton entra en el area de un objeto
+	/// </summary>
+	public override void _Process(double delta)
+	{
+		if (puedoMover)
+		{
 			Position = GetGlobalMousePosition();
 		}
 	}
-	private void _on_input_event(Node viewport, InputEvent @event, long shape_idx) {
-	 if(@event.IsActionPressed("click_izquierdo")){
+
+	/// <summary>
+    /// Este metodo es una señal del propio godot que nos permite detectar cuando haces click sobre le objeto
+    /// </summary>
+    /// <param name="viewport">Nodo del objeto parte de la API</param>
+    /// <param name="event">Nos permite detectar cuando se hace click</param>
+    /// <param name="shape_idx">Variable que se utiliza para la API</param>
+	private void _on_input_event(Node viewport, InputEvent @event, long shape_idx)
+	{
+		if (@event.IsActionPressed("click_izquierdo"))
+		{
 			puedoMover = true;
 			sprite.Visible = true;
 		}
-		if(@event.IsActionReleased("click_izquierdo")){
+		if (@event.IsActionReleased("click_izquierdo"))
+		{
 			puedoMover = false;
 		}
 	}
-	private void _on_area_entered(CollisionObject2D collisionObject2D) {
-			if (collisionObject2D.IsInGroup("GnomoSinCosas")&& !MueveTeEnElBosque.comenzar) {
-				sprite.Visible = false;
-			}
+
+	/// <summary>
+	/// Señal de godot que nos permite comprobar si la colision de un objeto colisiona contra otro objeto
+	/// </summary>
+	/// <param name="collisionObject2D">Variable de la colision del objeto que colisiona con el objeto</param>
+	private void _on_area_entered(CollisionObject2D collisionObject2D)
+	{
+		if (collisionObject2D.IsInGroup("GnomoSinCosas") && !MueveTeEnElBosque.comenzar)
+		{
+			sprite.Visible = false;
+		}
 	}
-	private void _on_area_exited(CollisionObject2D collisionObject2D) {
-			if (collisionObject2D.IsInGroup("GnomoSinCosas")&& !MueveTeEnElBosque.comenzar) {
-				sprite.Visible = true;
-			}
+
+	/// <summary>
+	/// Señal de godot que nos permite comprobar si la colision de un objeto deja de colisionar contra otro objeto
+	/// </summary>
+	/// <param name="collisionObject2D">Variable de la colision del objeto que colisiona con el objeto</param>
+	private void _on_area_exited(CollisionObject2D collisionObject2D)
+	{
+		if (collisionObject2D.IsInGroup("GnomoSinCosas") && !MueveTeEnElBosque.comenzar)
+		{
+			sprite.Visible = true;
+		}
 	}
 }
--- escenas/escena2/scripts/RespuestasRamon.cs
+++ escenas/escena2/scripts/RespuestasRamon.cs
@@ -1,36 +1,57 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Clase que nos permite gestionar las respuesta que da el jugador a Ramon
+/// </summary>
 public partial class RespuestasRamon : Area2D
 {
 	Sprite2D sprite1;
-    Sprite2D sprite2;
+	Sprite2D sprite2;
 	public static bool jugar = false;
 	public static bool pasar = false;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready(){
+	
+	/// <summary>
+	/// Esta funcion se llama automaticamente cuando se instancia el objeto al cual esta asociado el script
+	/// </summary>
+	public override void _Ready()
+	{
 		sprite1 = GetChild<Sprite2D>(0);
-        sprite2 = GetChild<Sprite2D>(1);
+		sprite2 = GetChild<Sprite2D>(1);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	/// <summary>
+	/// Señal de godot que nos permite saber cuando el raton entra en el area de un objeto
+	/// </summary>
+	void _on_mouse_entered()
 	{
-	}
-	void _on_mouse_entered(){
 		sprite2.Visible = false;
 	}
-	void _on_mouse_exited(){
+
+	/// <summary>
+	/// Señal de godot que nos permite saber cuando el raton sale en el area de un objeto
+	/// </summary>
+	void _on_mouse_exited()
+	{
 		sprite2.Visible = true;
 	}
 
-	void _on_input_event(Node viewport, InputEvent @event, long shape_idx) {
-		if(@event.IsActionPressed("click_izquierdo")&& this.IsInGroup("Si")){
+	/// <summary>
+    /// Este metodo es una señal del propio godot que nos permite detectar cuando haces click sobre le objeto
+    /// </summary>
+    /// <param name="viewport">Nodo del objeto parte de la API</param>
+    /// <param name="event">Nos permite detectar cuando se hace click</param>
+    /// <param name="shape_idx">Variable que se utiliza para la API</param>
+	void _on_input_event(Node viewport, InputEvent @event, long shape_idx)
+	{
+		if (@event.IsActionPressed("click_izquierdo") && this.IsInGroup("Si"))
+		{
 			jugar = true;
 			GetTree().ChangeSceneToFile("res://escenas/escena2.5/CartasCartitas.tscn");
 			GD.Print("Jugar");
 		}
-		else if(@event.IsActionPressed("click_izquierdo")&& this.IsInGroup("No")){
+		else if (@event.IsActionPressed("click_izquierdo") && this.IsInGroup("No"))
+		{
 			pasar = true;
 			GD.Print("Pasar");
 		}
