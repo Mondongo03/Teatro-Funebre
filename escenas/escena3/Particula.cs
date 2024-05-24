ddf6e48fc503dfa20752043d1ff2cfc43c7ac5ae
ruben <rjulian@elpuig.xeill.net>
2024-05-24 16:31:37 +0200

    Ritual Reconfigurado con las particulas

---
+++ escenas/escena3/Particula.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class Particula : Area2D {
+
+	public static bool rojo = false;
+	public static bool amarillo = false;
+	public static bool naranja = false;
+	public static bool verde = false;
+	public static bool blanco = false;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) {
+		encenderLuces();
+		apagarLuces();
+	}
+
+	public void encenderLuces(){
+		if(this.IsInGroup("Rojo") && rojo) this.Visible = true;
+		if(this.IsInGroup("Amarillo") && amarillo) this.Visible = true;
+		if(this.IsInGroup("Naranja") && naranja) this.Visible = true;
+		if(this.IsInGroup("Verde") && verde) this.Visible = true;
+		if(this.IsInGroup("Blanco") && rojo) this.Visible = true;
+	}
+	public void apagarLuces(){
+		if(this.IsInGroup("Rojo") && !rojo) this.Visible = false;
+		if(this.IsInGroup("Amarillo") && !amarillo) this.Visible = false;
+		if(this.IsInGroup("Naranja") && !naranja) this.Visible = false;
+		if(this.IsInGroup("Verde") && !verde) this.Visible = false;
+		if(this.IsInGroup("Blanco") && !blanco) this.Visible = false;
+	}
+}
--- escenas/escena3/scripts/Ritual.cs
+++ escenas/escena3/scripts/Ritual.cs
@@ -22,19 +22,24 @@ public partial class Ritual : Area2D {
 	public void _on_area_entered(Area2D area) {
         if(this.IsInGroup("Arriba") && area.IsInGroup("UnicornioRojo")){
 			slotCorrecto++;
+			Particula.rojo = true;
 		}
 		 if(this.IsInGroup("Derecha") && area.IsInGroup("UnicornioVerde")){
 			slotCorrecto++;
+			Particula.verde = true;
 		}
 		 if(this.IsInGroup("Izquierda") && area.IsInGroup("UnicornioAmarillo")){
 			slotCorrecto++;
+			Particula.amarillo = true;
 		}
 		
 		 if(this.IsInGroup("AbajoIzquierda") && area.IsInGroup("UnicornioNaranja")){
 			slotCorrecto++;
+			Particula.naranja = true;
 		}
 		if(this.IsInGroup("AbajoDerecha") && area.IsInGroup("UnicornioBlanco")){
 			slotCorrecto++;
+			Particula.blanco = true;
 		}
 		
     }
@@ -42,19 +47,24 @@ public partial class Ritual : Area2D {
 	public void _on_area_exited(Area2D area) {
         if(this.IsInGroup("Arriba") && area.IsInGroup("UnicornioRojo")){
 			slotCorrecto--;
+			Particula.rojo = false;
 		}
 		 if(this.IsInGroup("Derecha") && area.IsInGroup("UnicornioVerde")){
 			slotCorrecto--;
+			Particula.verde = false;
 		}
 		 if(this.IsInGroup("Izquierda") && area.IsInGroup("UnicornioAmarillo")){
 			slotCorrecto--;
+			Particula.amarillo = false;
 		}
 		
 		 if(this.IsInGroup("AbajoIzquierda") && area.IsInGroup("UnicornioNaranja")){
 			slotCorrecto--;
+			Particula.naranja = false;
 		}
 		if(this.IsInGroup("AbajoDerecha") && area.IsInGroup("UnicornioBlanco")){
 			slotCorrecto--;
+			Particula.blanco = false;
 		}
 		
     }
